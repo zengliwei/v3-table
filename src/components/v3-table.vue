3af394205e60236f1f51e5fe095a0b073972da62
zengliwei <zengliwei@163.com>
2024-09-28 02:34:51 +0800

    + Add source files

--- src/components/v3-table.vue
+++ src/components/v3-table.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {defineEmits, defineProps, defineComponent, ref, reactive, computed, watch} from 'vue';
+import {defineEmits, defineProps, defineComponent, ref, reactive, computed, watch, onMounted} from 'vue';
 import v3TableTr from './v3-table-tr.vue';
 
 defineEmits(['row-click', 'row-db-click']);
@@ -33,8 +33,26 @@ const props = defineProps({
     type: String
   },
 
+  srcMethod: {
+    type: String,
+    default: 'GET'
+  },
+
   srcConditions: {
     type: Array
+  },
+
+  srcHandler: {
+    type: Function
+  },
+
+  height: {
+    type: String
+  },
+
+  autoLoad: {
+    type: Boolean,
+    default: true
   },
 
   pageSize: {
@@ -77,31 +95,37 @@ for (let key in props.filterTypes) {
   defineComponent(props.filterTypes[key]);
 }
 
-let cols = ref([]),
+let cols = reactive([]),
     rows = reactive([]),
     filteredRows = reactive([]),
     sourceRows = reactive([]),
     activatedRows = reactive([]),
     filters = reactive([]),
-    showAutoWidthCol = ref(false),
-    lastLeftFixedColIdx = ref(-1),
-    rowTotal = ref(0),
-    page = ref(1),
-    pageCount = ref(1),
+    showAutoWidthCol = false,
+    lastLeftFixedColIdx = -1,
+    rowTotal = 0,
+    page = 1,
+    pageCount = 1,
     pages = computed(() => {
       let tmpPages = [];
-      for (let p = 1; p <= pageCount.value; p++) {
+      for (let p = 1; p <= pageCount; p++) {
         tmpPages.push(p);
       }
       return tmpPages;
     }),
     pagingMsg = computed(() => {
       return props.pagingMsg
-          .replace(':page_count', pageCount.value)
-          .replace(':page', page.value)
-          .replace(':row_total', rowTotal.value);
+          .replace(':page_count', pageCount)
+          .replace(':page', page)
+          .replace(':row_total', rowTotal);
     }),
     sortOrders = [];
+
+const elTable = ref(),
+    elMain = ref(),
+    elHeader = ref(),
+    elBody = ref(),
+    elFooter = ref();
 
 watch(
     () => props.columns,
@@ -199,12 +223,6 @@ const checkAll = function () {
   console.log('checkAll');
 };
 
-const updateData = function (data, total) {
-  rows = data;
-  rowTotal = total || data.length;
-  pageCount = Math.ceil(total / props.pageSize);
-};
-
 const filterByLocal = function () {
   filteredRows = sourceRows.filter((row) => {
     filters.forEach((filter) => {
@@ -226,7 +244,16 @@ const filterByLocal = function () {
   page = 1;
 };
 
-const filterByRemote = function () {
+const refreshByRemote = function () {
+  fetch(props.srcUrl, {
+    method: props.srcMethod,
+    params: {
+      c: props.columns.map((col) => col['field']),
+      f: filters,
+      p: page,
+      ps: props.pageSize
+    }
+  });
 };
 
 const sortByLocal = function () {
@@ -267,86 +294,108 @@ const sort = function (col) {
   col['cssClass']['desc'] = col['sortDir'] === 'desc';
 };
 
+const updateData = function (data, total) {
+  rows = data;
+  rowTotal = total || data.length;
+  pageCount = Math.ceil(total / props.pageSize);
+};
+
 const updatePage = function (p) {
   if (p < 1 || p > pageCount) return;
-  if (props.srcUrl) {
-    filterByRemote();
+  if (props.srcHandler) {
+    refreshByRemote();
   } else {
     page = p;
     updateData(filteredRows.slice((page - 1) * props.pageSize, page * props.pageSize), filteredRows.length);
   }
 };
+
+if (props.autoLoad) {
+  if (props.srcUrl) {
+    refreshByRemote();
+  } else {
+    updateData(props.data, props.data.length);
+  }
+}
+
+onMounted(() => {
+  if (props.height) {
+    elMain.value.style.height = `calc(100% - ${elFooter.value.offsetHeight}px)`;
+  }
+});
 </script>
 
 <template>
-  <div class="v3-table">
-    <div class="v3-table-header">
-      <table>
-        <thead>
-        <tr>
-          <template v-for="(col, c) in cols">
-            <th v-if="col['type'] === 'checkbox'"
-                class="checkbox" :style="col['style']">
-              <div><input type="checkbox" @click="checkAll"/></div>
-            </th>
-            <th v-if="col['type'] === 'index'"
-                class="index" :style="col['style']">
-              <div>#</div>
-            </th>
-            <th v-if="col['type'] === 'data' && !col['hidden']"
-                :class="col['cssClass']" :style="col['style']">
-              <div @click="sort(col)"><span v-text="col['title']"></span></div>
-            </th>
-          </template>
-          <th v-if="showAutoWidthCol" class="auto"></th>
-          <th v-if="rowActions.length > 0" class="actions"></th>
-        </tr>
-        <tr v-if="showColumnFilter">
-          <template v-for="(col, c) in cols">
-            <th v-if="col['type'] === 'checkbox'" class="checkbox"></th>
-            <th v-if="col['type'] === 'index'" class="index"></th>
-            <th v-if="col['type'] === 'data' && !col['hidden']">
-              <component
-                  v-if="col['filter']"
-                  :is="col['filter']['type']"
-                  :params="col['filter']['params']"></component>
-            </th>
-          </template>
-          <th v-if="showAutoWidthCol"></th>
-          <th v-if="rowActions.length > 0"></th>
-        </tr>
-        </thead>
-      </table>
-    </div>
-    <div class="v3-table-body">
-      <table>
-        <tbody>
-        <template v-if="rows.length > 0">
-          <template v-for="(row, r) in rows">
-            <v3-table-tr
-                :idx="r"
-                :row="row"
-                :cols="cols"
-                :row-actions="rowActions"
-                :show-auto-width-col="showAutoWidthCol"
-                :last-left-fixed-col-idx="lastLeftFixedColIdx"
-                :activated-rows="activatedRows"
-                @click="(obj) => $emit('row-click', obj)"
-                @db-click="(obj) => $emit('row-db-click', obj)"></v3-table-tr>
-          </template>
-        </template>
-        <template v-else>
+  <div class="v3-table" :style="{height: height}" ref="elTable">
+    <div class="v3-table-main" ref="elMain">
+      <div class="v3-table-header" ref="elHeader">
+        <table>
+          <thead>
           <tr>
-            <td :colspan="cols.length"
-                class="no-data">
-              <div v-text="tipNoData"></div>
-            </td>
+            <template v-for="(col, c) in cols">
+              <th v-if="col['type'] === 'checkbox'"
+                  class="checkbox" :style="col['style']">
+                <div><input type="checkbox" @click="checkAll"/></div>
+              </th>
+              <th v-if="col['type'] === 'index'"
+                  class="index" :style="col['style']">
+                <div>#</div>
+              </th>
+              <th v-if="col['type'] === 'data' && !col['hidden']"
+                  :class="col['cssClass']" :style="col['style']">
+                <div @click="sort(col)"><span v-text="col['title']"></span></div>
+              </th>
+            </template>
+            <th v-if="showAutoWidthCol" class="auto"></th>
+            <th v-if="rowActions.length > 0" class="actions"></th>
           </tr>
-        </template>
-        </tbody>
-      </table>
+          <tr v-if="showColumnFilter">
+            <template v-for="(col, c) in cols">
+              <th v-if="col['type'] === 'checkbox'" class="checkbox"></th>
+              <th v-if="col['type'] === 'index'" class="index"></th>
+              <th v-if="col['type'] === 'data' && !col['hidden']">
+                <component
+                    v-if="col['filter']"
+                    :is="col['filter']['type']"
+                    :params="col['filter']['params']"></component>
+              </th>
+            </template>
+            <th v-if="showAutoWidthCol"></th>
+            <th v-if="rowActions.length > 0"></th>
+          </tr>
+          </thead>
+        </table>
+      </div>
+      <div class="v3-table-body" ref="elBody">
+        <table>
+          <tbody>
+          <template v-if="rows.length > 0">
+            <template v-for="(row, r) in rows">
+              <v3-table-tr
+                  :idx="r"
+                  :row="row"
+                  :cols="cols"
+                  :row-actions="rowActions"
+                  :show-auto-width-col="showAutoWidthCol"
+                  :last-left-fixed-col-idx="lastLeftFixedColIdx"
+                  :activated-rows="activatedRows"
+                  @click="(obj) => $emit('row-click', obj)"
+                  @db-click="(obj) => $emit('row-db-click', obj)"></v3-table-tr>
+            </template>
+          </template>
+          <template v-else>
+            <tr>
+              <td :colspan="cols.length"
+                  class="no-data">
+                <div v-text="tipNoData"></div>
+              </td>
+            </tr>
+          </template>
+          </tbody>
+        </table>
+      </div>
     </div>
-    <div class="v3-table-footer">
+    <div class="v3-table-footer" ref="elFooter">
       <div></div>
       <div v-text="pagingMsg"></div>
       <div class="paginator">
@@ -387,8 +436,8 @@ const updatePage = function (p) {
 
   --v3-table-row-bg: #fff;
   --v3-table-row-even-bg: #f6f6f7;
-  --v3-table-row-hover-bg: #f4f5f8;
-  --v3-table-row-activated-bg: #f1f3f6;
+  --v3-table-row-hover-bg: #fff3e0;
+  --v3-table-row-activated-bg: #e3f2fd;
 
   --v3-table-footer-bg: #f4f5f8;
   --v3-table-footer-padding: .5rem 1rem;
@@ -408,65 +457,19 @@ const updatePage = function (p) {
 }
 
 .v3-table {
-  overflow: auto;
-  position: relative;
+  border-color: var(--v3-table-border-color);
+  border-style: solid;
+  border-width: 1px;
   width: var(--v3-table-width);
 }
 
-.v3-table a {
-  color: var(--v3-table-link-color);
-  text-decoration: none;
-}
-
-.v3-table select {
-  border: 1px solid var(--v3-table-border-color);
+.v3-table * {
   box-sizing: border-box;
-  color: inherit;
-  display: block;
-  font-size: inherit;
-  outline: none;
-  padding: var(--v3-table-select-padding);
-  width: 100%;
 }
 
-.v3-table table {
-  border-color: var(--v3-table-border-color);
-  border-style: solid;
-  border-width: 1px 0 0 1px;
-  border-collapse: separate;
-  border-spacing: 0;
-  table-layout: fixed;
-  width: 100%;
-}
-
-.v3-table th,
-.v3-table td {
-  border-color: var(--v3-table-border-color);
-  border-style: solid;
-  border-top-width: 0;
-  border-left-width: 0;
-  border-bottom-width: 1px;
-  border-right-width: 1px;
-  box-sizing: border-box;
-  line-height: var(--v3-table-line-height);
-  padding: var(--v3-table-cell-padding);
-}
-
-.v3-table th:last-child,
-.v3-table td:last-child {
-  border-right-width: 1px;
-}
-
-.v3-table th.checkbox > div,
-.v3-table td.checkbox > div {
-  justify-content: center;
-}
-
-.v3-table th > div,
-.v3-table td > div {
-  display: flex;
-  align-content: center;
-  padding: var(--v3-table-cell-box-padding);
+.v3-table .v3-table-main {
+  overflow: auto;
+  position: relative;
 }
 
 .v3-table .v3-table-header {
@@ -516,8 +519,8 @@ const updatePage = function (p) {
   background-image: var(--v3-table-header-sort-desc-icon);
 }
 
-.v3-table .v3-table-body table {
-  border-top: 0;
+.v3-table .v3-table-body {
+  z-index: 1;
 }
 
 .v3-table .v3-table-body tr {
@@ -572,7 +575,7 @@ const updatePage = function (p) {
   background: var(--v3-table-footer-bg);
   border-color: var(--v3-table-border-color);
   border-style: solid;
-  border-width: 0 1px 1px 1px;
+  border-width: 1px 0 0 0;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -596,5 +599,56 @@ const updatePage = function (p) {
 
 .v3-table .v3-table-footer .paginator select {
   padding: var(--v3-table-paginator-select-padding);
+}
+
+.v3-table a {
+  color: var(--v3-table-link-color);
+  text-decoration: none;
+}
+
+.v3-table select {
+  border: 1px solid var(--v3-table-border-color);
+  color: inherit;
+  display: block;
+  font-size: inherit;
+  outline: none;
+  padding: var(--v3-table-select-padding);
+  width: 100%;
+}
+
+.v3-table table {
+  border-collapse: separate;
+  border-spacing: 0;
+  table-layout: fixed;
+  width: 100%;
+}
+
+.v3-table th,
+.v3-table td {
+  border-color: var(--v3-table-border-color);
+  border-style: solid;
+  border-top-width: 0;
+  border-left-width: 0;
+  border-bottom-width: 1px;
+  border-right-width: 1px;
+  line-height: var(--v3-table-line-height);
+  padding: var(--v3-table-cell-padding);
+}
+
+.v3-table th:last-child,
+.v3-table td:last-child {
+  border-right-width: 1px;
+}
+
+.v3-table th.checkbox > div,
+.v3-table td.checkbox > div {
+  justify-content: center;
+}
+
+.v3-table th > div,
+.v3-table td > div {
+  display: flex;
+  align-content: center;
+  padding: var(--v3-table-cell-box-padding);
 }
 </style>
